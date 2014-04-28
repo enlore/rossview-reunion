fc645a47e5db16772465006df8eb0a6ca32103fe
enlore <n.e.lorenson@gmail.com>
2014-04-27 20:13:42 -0500

    Assets, requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from forms import ApplicationForm
+from flask.ext.assets import Environment, Bundle
 
 def create_app(instance_path=None, debug=False):
     use_instances = False
@@ -20,6 +21,14 @@ def create_app(instance_path=None, debug=False):
     app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
     app.config.from_pyfile('config.py')
     app.config.from_object('app.config')
+
+    assets = Environment(app)
+    
+    css_bundle = Bundle('less/main.less', output='css/main.css', filters='less')
+    assets.register('css', css_bundle)
+
+    js_bundle = Bundle('js/main.js', output="main.min.js", filters="rjsmin")
+    assets.register('js', js_bundle)
 
     @app.route('/', methods=['GET', 'POST'])
     def index():
--- app/forms.py
+++ app/forms.py
@@ -1,26 +1,30 @@
 from flask_wtf import Form
-from wtforms import TextField
+from wtforms import TextField, HiddenField, SubmitField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import Required, EqualTo
 
 
 class ApplicationForm(Form):
+    squirrel            = TextField("")
+
     name_msg            = "We need to know who you are."
     name                = TextField("Your name", [Required(name_msg)])
 
     email_msg           = "Your email is required."
     email               = EmailField('Your Email', [Required(email_msg)])
 
-    confirm_email_req   = "Please confirm your email address."
-    confirm_email_msg   = "Check to be sure your email matches."
-    confirm_email       = EmailField('Confirm Your Email', 
+    email_confirm_req   = "Please confirm your email address."
+    email_confirm_msg   = "Check to be sure your email matches."
+    email_confirm       = EmailField('Confirm Your Email', 
                             [
-                                Required(confirm_email_req),
-                                EqualTo('confirm_email', message=confirm_email_msg)
+                                Required(email_confirm_req),
+                                EqualTo('confirm_email', message=email_confirm_msg)
                             ])
 
     name_msg            = "Please provide your full name. This helps us find you faster!"
     name                = TextField("Full name", [Required(name_msg)])
 
     phone_message       = "We need your phone number, please."
-    phone               = TextField("Your phone number", [Required(phone_message)])
+    phone               = TextField("Your Phone Number", [Required(phone_message)])
+
+    submit              = SubmitField("Send Your Application")
