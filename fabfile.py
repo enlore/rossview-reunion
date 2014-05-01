fd3557f93ace667e1ad9f0a803a561b94d116348
enlore <n.e.lorenson@gmail.com>
2014-05-01 18:46:36 -0500

    Boilerplate

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,39 @@ import os
 cwd = os.getcwd()
 instance_path = os.path.join(cwd, 'instance')
 
+env.hosts = ['198.23.165.64']
+env.user = 'no'
+
 def d():
     """Run in debug mode"""
     local('python manage.py run -i {} -D true'.format(instance_path))
+
+def pack():
+    local('python setup.py sdist --formats=gztar')
+
+def deploy():
+    name = local('python setup.py --fullname', capture=True)
+
+    put('dist/{}.tar.gz'.format(name), '/tmp/{}.tar.gz'.format(name))
+
+    with cd('/tmp'):
+        run('tar xvzf {}.tar.gz'.format(name))
+
+        with cd('{}'.format(name)):
+            run('/home/no/.virtualenvs/rossview.reunion/bin/python setup.py install')
+
+        run('rm -rf {} {}.tar.gz'.format(name, name))
+
+    # make or update timestamp of instance config
+    run('touch /var/www/rhs04reunion.com/instance/config.py')
+
+def restart_remote():
+    # stop app server
+    # start app server
+    print "IMPLEMENT ME"
+
+def ship():
+    pack()
+    deploy()
+    restart_remote()
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+       name="rossview.reunion.site",
+       version="1.0.0",
+       description="Simple form processing site",
+       author="enlore",
+       author_email="n.e.lorenson@gmail.com",
+       url="",
+       packages=["app"],
+       zip_safe=False,
+       include_package_data=True,
+       install_requires=[
+           "Fabric==1.8.3",
+           "Flask==0.10.1",
+           "Flask-Assets==0.9",
+           "Flask-Script==0.6.7",
+           "Flask-WTF==0.9.5",
+           "Jinja2==2.7.2",
+           "MarkupSafe==0.21",
+           "WTForms==1.0.5",
+           "Werkzeug==0.9.4",
+           "argparse==1.2.1",
+           "ecdsa==0.11",
+           "ipython==2.0.0",
+           "itsdangerous==0.24",
+           "paramiko==1.12.3",
+           "py==1.4.20",
+           "pycrypto==2.6.1",
+           "pyjade==2.2.0",
+           "pytest==2.5.2",
+           "requests==2.2.1",
+           "six==1.6.1",
+           "webassets==0.9",
+           "wsgiref==0.1.2"
+           ]
+        )
